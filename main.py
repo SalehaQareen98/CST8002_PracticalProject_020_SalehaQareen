399ab342b6976d52d94ee8336edbf446bdfaede2
SalehaQareen98 <salehaqareen@gmail.com>
2026-01-27 14:13:05 -0500

    1.2.5 - Store record objects in an array or list data structure

--- main.py
+++ main.py
@@ -8,11 +8,13 @@ print("Program Author: Saleha Qareen\n")
 
 csv_path = r"C:\Users\saleh\Documents\Level 6\python\CST8002_PracticalProject_020_SalehaQareen\pacific_rim_npr_coastalmarine_black_oystercatcher_population_nesting_counts_2008-2017_data.csv"
 
-records = []  # array / list to store record objects
+# array / list to store record objects
 
+records = []  
+# Implement File I/O to open and read the CSV dataset at program startup
 with open(csv_path, newline="", encoding="utf-8") as file:
     reader = csv.DictReader(file)
-
+    # Parse dataset records into individual data fields 
     for row in reader:
         record = OystercatcherRecord(
             row["Visit date"],
@@ -20,3 +22,7 @@ with open(csv_path, newline="", encoding="utf-8") as file:
             row["Species"],
             row["Total Black oystercatcher adults"]
         )
+        
+        # Store record objects in an array or list data structure
+        records.append(record)
+        # print(record)
